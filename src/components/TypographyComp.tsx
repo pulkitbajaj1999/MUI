f5869aef5a96ef4acfca8fcc5f2129c823554148
pulkit171112236 <pulkit171112236@gmail.com>
2023-07-13 12:03:45 +0530

    typography component

---
+++ src/components/TypographyComp.tsx
@@ -0,0 +1,16 @@
+import { Typography } from '@mui/material'
+
+const TypographyComp = () => {
+  return (
+    <div>
+      <Typography variant="h1">heading 1</Typography>
+      <Typography variant="h2">heading 2</Typography>
+      <Typography variant="h3">heading 3</Typography>
+      <Typography variant="h4">heading 4</Typography>
+      <Typography variant="h5">heading 5</Typography>
+      <Typography variant="h6">heading 6</Typography>
+    </div>
+  )
+}
+
+export default TypographyComp
